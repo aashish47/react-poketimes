7f6e035f17ecbed26bf7089ccd2ee153594f5378
Aashish Agarwal <aashish27nov@gmail.com>
2019-03-01 13:19:29 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, alias accusantium harum vero ipsa dolorem repudiandae, rem sapiente odio explicabo quaerat illo quod itaque repellat ipsum cum quisquam magni ducimus!</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, alias accusantium harum vero ipsa dolorem repudiandae, rem sapiente odio explicabo quaerat illo quod itaque repellat ipsum cum quisquam magni ducimus!</p>
+        </div>
+    )
+}
+
+export default Contact

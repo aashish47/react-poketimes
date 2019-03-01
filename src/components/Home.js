4ba9bc765292acac0ffcc8d604dc172cc86ea794
Aashish Agarwal <aashish27nov@gmail.com>
2019-03-01 22:08:34 +0530

    route parameters

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React , {Component} from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component{
     // setTimeout(() => {
@@ -26,8 +28,11 @@ class Home extends Component{
             posts.map(post => {
                 return(
                     <div className="post card" key = {post.id}>
-                        <div className="card content">
-                            <span className="card-title">{post.title}</span>
+                        <img src={Pokeball} alt="A pokeball"/>
+                        <div className="card-content">
+                            <Link to={'/' +post.id}>
+                                <span className="card-title red-text">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -37,7 +42,7 @@ class Home extends Component{
             <div className="center">No Posts yet!!!</div>
         )
         return(
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
